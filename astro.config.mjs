7fed6c7fda24eff41cccaa086c9d9fae368c1ae5
RBK Blansko <rbkblansko@gmail.com>
2026-04-27 21:34:22 +0200

    fix domain config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://github.io', // Placeholder, typically https://username.github.io
-  base: '/rbkblansko-web',
+  site: 'https://www.rbkblansko.cz',
+  base: '/',
   integrations: [tailwind()],
 });
